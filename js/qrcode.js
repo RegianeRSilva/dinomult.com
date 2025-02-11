34f94abb93afe35be7444eb508e7b6015937e2ca
Regiane Rodrigues <regianes2rodrigues@outlook.com>
2025-02-11 19:48:21 -0300

    atualização

--- js/qrcode.js
+++ js/qrcode.js
@@ -7,7 +7,9 @@ generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value;
     if(!qrValue) return;  // se a entrada estiver vazia, retorne daqui
     qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=190x190&data=${qrValue}';
-    qrImg.addEventListener("load", () => )
-    wrapperqr.classList.add("active");
+    qrImg.addEventListener("load", () => {
+        wrapperqr.classList.add("active");
 
+    });
+    
 });
