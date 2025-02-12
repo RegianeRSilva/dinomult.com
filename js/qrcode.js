f34e26a8485ea365ad31c75f786e32366606b5f6
Regiane Rodrigues <regianes2rodrigues@outlook.com>
2025-02-11 22:45:06 -0300

    finalizando qr script

--- js/qrcode.js
+++ js/qrcode.js
@@ -6,10 +6,17 @@ qrImg = wrapperqr.querySelector(".qr-code img");
 generateBtn.addEventListener("click", () => {
     let qrValue = qrInput.value;
     if(!qrValue) return;  // se a entrada estiver vazia, retorne daqui
+    generateBtn.innerText = "Gerando QR Code...";
     qrImg.src = 'https://api.qrserver.com/v1/create-qr-code/?size=190x190&data=${qrValue}';
     qrImg.addEventListener("load", () => {
         wrapperqr.classList.add("active");
-
+        generateBtn.innerText = "Gerar QR Code";
     });
     
+});
+
+qrInput.addEventListener("keyup", () => {
+    if(!qrInput.value){
+        wrapperqr.classList.remove("active");
+    }
 });
